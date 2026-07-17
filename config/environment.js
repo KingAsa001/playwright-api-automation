f37d6487365612a149cc76efa279b4025fa12ea9
KingAsa001 <stephenaina16@gmail.com>
2026-07-17 12:54:06 +0100

    Add environment configuration

--- config/environment.js
+++ config/environment.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 module.exports = {
     baseURL: process.env.BASE_URL,
     username: process.env.USERNAME,
-    password: process.env.PASSWORD
+    password: process.env.PASSWORD,
+    token: process.env.TOKEN
 };
